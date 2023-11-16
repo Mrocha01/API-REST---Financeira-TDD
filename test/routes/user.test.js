362d0fe18ccf19f7a49a80b253b26a3ad5d678f7
MateusR <mateusftc_@hotmail.com>
2023-11-15 21:36:56 -0300

    refactor: Testes ajustados com interação ao banco

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,21 +1,22 @@
 const request = require("supertest");
 
-const app = require("../src/app.js");
+const app = require("../../src/app.js");
 
 test("Deve listar todos os usuarios", () => {
   return request(app)
     .get("/users")
     .then((res) => {
       expect(res.statusCode).toBe(200);
-      expect(res.body).toHaveLength(1);
-      expect(res.body[0]).toHaveProperty("name", "John Doe");
+      expect(res.body.length).toBeGreaterThan(0);
     });
 });
 
 test("Deve inserir usuario com sucesso", () => {
+  const mail = `${Date.now()}@mail.com`;
+  
   return request(app)
     .post("/users")
-    .send({ name: "Walter Mitty", mail: "walter@mail.com" })
+    .send({ name: "Walter Mitty", email: mail, passwd:"12345"})
     .then((res) => {
       expect(res.statusCode).toBe(201);
       expect(res.body.name).toBe("Walter Mitty");
