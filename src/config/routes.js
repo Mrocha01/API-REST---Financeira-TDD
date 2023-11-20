2f7eecc744eab1f1bf8159daa00dc368a8c68572
MateusR <mateusftc_@hotmail.com>
2023-11-19 21:32:40 -0300

    feat: Rotas/Services/Testes para tabela Accounts

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,4 +5,8 @@ module.exports = (app) => {
     .route("/users")
     .get(app.routes.users.findAll)
     .post(app.routes.users.create);
+
+  app
+    .route("/accounts")
+    .post(app.routes.accounts.create);
 };
