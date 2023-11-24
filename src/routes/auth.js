1261ef63d7cec3c814e1b30850642496b32ec23d
MateusR <mateusftc_@hotmail.com>
2023-11-23 22:34:43 -0300

    feat: Middleware de criação do Token

---
+++ src/routes/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jwt-simple");
+const bcrypt = require("bcrypt");
+
+const secret = "Segredo!";
+
+module.exports = (app) => {
+    const signin = (req, res, next) => {
+        app.services.user
+        .findOne({email: req.body.email})
+        .then((user) => {
+            if(bcrypt.compareSync(req.body.passwd, user.passwd)){
+                const payload = {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                };
+                const token = jwt.encode(payload, secret);
+                res.status(200).json({token});
+            }
+        })
+        .catch((err) => {
+            return next(err);
+        });
+    };
+
+    return { signin };
+};
