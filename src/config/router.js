18533e425d2d769eb2bc95023fe8de0c679adade
MateusR <mateusftc_@hotmail.com>
2023-11-26 21:03:59 -0300

    refactor: Migrando Users para roteamento modular

--- src/config/router.js
+++ src/config/router.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
 module.exports = (app) => {
-    app.use("/auth", app.routes.auth)
+    app.use("/auth", app.routes.auth);
+    app.use("/users", app.routes.users);
+    app.use("/accounts", app.routes.accounts);
+
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,10 @@
 // Controller
+const express = require('express');
 
 module.exports = (app) => {
-  const findAll = (req, res, next) => {
+  const router = express.Router();
+
+  router.get("/", (req, res, next) => {
     app.services.user.findAll()
       .then(result => {
       res.status(200).json(result);
@@ -9,9 +12,9 @@ module.exports = (app) => {
     .catch((err) => {
       next(err);
     });
-  };
+  });
 
-  const create = async (req, res, next) => {
+  router.post("/", async(req, res, next) => {
     try {
       const result = await app.services.user.save(req.body);
 
@@ -20,7 +23,7 @@ module.exports = (app) => {
     } catch (err) {
         return next(err);
     }
-  };
+  });
 
-  return { findAll, create };
+  return router;
 };
