7570206947efb3c395c7c17c609376a986e23541
MateusR <mateusftc_@hotmail.com>
2023-11-28 22:02:40 -0300

    refactor: Validação de rotas com param ID
    Criado middleware próprio para lidar com essas rotas especificas

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,8 +1,22 @@
 // Controller
 const express = require('express');
+const RecursoIndevidoError = require("../errors/RecursoIndevidoError");
 
 module.exports = (app) => {  
     const router = express.Router();
+
+    router.param("id", (req, res, next) => {
+        app.services.account.findOne(req.params.id)
+        .then((result) => {
+            if(result.user_id !== req.user.id){
+                throw new RecursoIndevidoError();
+            }
+            return next();
+        })
+        .catch((err) => {
+            next(err);
+        });
+    });
 
     router.post("/", (req, res, next) => {
         app.services.account.save({ ...req.body, user_id: req.user.id })
@@ -27,10 +41,6 @@ module.exports = (app) => {
     router.get("/:id",(req, res, next) => {
         app.services.account.findOne(req.params.id)
             .then((result) => {
-                if(result.user_id !== req.user.id){
-                    return res.status(403)
-                    .json({ error: "Este recurso não pertence ao usuário!"})
-                }
                 return res.status(200).json(result);
             })
             .catch((err) => {
